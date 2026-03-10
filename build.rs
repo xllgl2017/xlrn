eda49b3aabdbc73ec28da442beb4fa651cc6f557
XLX <xinlanxi2017@gmail.com>
2026-03-10 15:12:52 +0800

    release 0.1.0

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
-use std::env;
+use std::{env, fs};
+use std::path::Path;
 
 fn main() {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
@@ -10,7 +11,15 @@ fn main() {
     }
     println!("cargo:rustc-link-lib={}=xlnt", typ);
     match (target_os.as_str(), target_env.as_str()) {
-        ("windows", "msvc") => println!("cargo:rustc-link-lib=advapi32"),
+        ("windows", "msvc") => {
+            let out_dir = env::var("OUT_DIR").unwrap();
+            let path = Path::new(&out_dir).parent().unwrap().parent().unwrap().parent().unwrap();
+            let dll_src = format!("{}/lib/{}/{}/xlnt.dll", manifest_dir, target_os, target_env);
+            let dll_dst =path.join("xlnt.dll");
+            fs::copy(&dll_src, &dll_dst).expect("Failed to copy DLL");
+
+            //println!("cargo:rustc-link-lib=advapi32")
+        }
         ("windows", "gnu") => {
             // println!("cargo:rustc-link-lib=dylib=advapi32");
             // println!("cargo:rustc-link-lib=stdc++");
